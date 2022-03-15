43f0dd719cc7e7b935154986ddd7cf57219cd046
brightseon <public.seon@gmail.com>
2022-03-15 18:37:25 +0900

    1.6 Custom App

--- components/NavBar.js
+++ components/NavBar.js
@@ -15,16 +15,12 @@ export default function NavBar() {
                 </a>
             </Link>
             <style jsx>{`
-                nav {
-                    background-color: tomato;
-                }
-
                 a {
                     text-decoration: none;
                 }
 
                 .active {
-                    color: yellow;
+                    color: tomato;
                 }
             `}</style>
         </nav>
--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,7 @@ import NavBar from '../components/NavBar';
 export default function Home() {
     return (
         <div>
-            <NavBar />
             <h1>Hello</h1>
-            <style jsx>{`
-                a {
-                    color: white;
-                }
-            `}</style>
         </div>
     );
 }
