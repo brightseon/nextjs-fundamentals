83c8862b15cf5b2cea03e55aa38ab2fdd0d5f33f
brightseon <public.seon@gmail.com>
2022-03-15 19:05:44 +0900

    2.0 Patterns

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
-import NavBar from '../components/NavBar';
+import Seo from '../components/Seo';
 
 export default function Home() {
     return (
         <div>
-            <h1>Hello</h1>
+            <Seo title={'Home'} />
+            <h1 className="active">Hello</h1>
         </div>
     );
 }
