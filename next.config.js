9c9edf4f5751ed58a893fd0da15ce71f0c0566b0
brightseon <public.seon@gmail.com>
2022-03-17 18:41:15 +0900

    2.2 Redirect and Rewrite

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const API_KEY = process.env.API_KEY;
 
-module.exports = nextConfig
+const nextConfig = {
+    reactStrictMode: true,
+    async redirects() {
+        return [
+            {
+                source: '/old-blog/:path*',
+                destination: '/new-blog/:path*',
+                permanent: false,
+            },
+        ];
+    },
+    async rewrites() {
+        return [
+            {
+                source: '/api/movies',
+                destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+            },
+        ];
+    },
+};
+
+module.exports = nextConfig;
