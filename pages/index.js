3a06de3a9644f11054ba8242182d24a92e09a932
brightseon <public.seon@gmail.com>
2022-03-23 18:40:16 +0900

    2.7 Catch All

--- pages/index.js
+++ pages/index.js
@@ -6,15 +6,7 @@ export default function Home({ results }) {
     const router = useRouter();
 
     const onClick = (id, title) => {
-        router.push(
-            {
-                pathname: `/movies/${id}`,
-                query: {
-                    title
-                }
-            },
-            `/movies/${id}`
-        );
+        router.push(`/movies/${title}/${id}`);
     };
 
     return (
@@ -28,15 +20,7 @@ export default function Home({ results }) {
                 >
                     <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
                     <h4>
-                        <Link
-                            href={{
-                                pathname: `/movies/${movie.id}`,
-                                query: {
-                                    title: movie.original_title
-                                }
-                            }}
-                            as={movie.original_title}
-                        >
+                        <Link href={`/movies/${movie.original_title}/${movie.id}`}>
                             <a>{movie.original_title}</a>
                         </Link>
                     </h4>
